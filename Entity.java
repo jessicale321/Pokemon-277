09cc636f0bdc3f969f7d288a6cfe4d6f3be0a2dc
Yisacorr <91310369+Yisacorr@users.noreply.github.com>
2021-10-04 00:04:50 -0700

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -1,1 +1,25 @@
+public class Entity {
+    
+    private String name;
+    private int hp;
+    private int maxHp;
+    
+    public Entity(String n, int mHp){}
+    
+    public int getHp(){return hp;}
+    
+    public int getMaxHp(){return maxHp;}
+    
+    public void takeDamage(int d){}
+    
+    public void heal(){}
+    
+    public String getName(){return name;}
+    
+
+
+    @Override
+    public String toString(){return (name + "HP: " + hp + "/" + maxHp);} 
+
+}
 
