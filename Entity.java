9d23d04703c2c90ca24d5c4ff3fc4eb6908b31aa
Yisacorr <91310369+Yisacorr@users.noreply.github.com>
2021-10-04 20:28:50 -0700

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -7,22 +7,38 @@ public class Entity {
     public Entity(String n, int mHp){
         name = n;
         maxHp = mHp;
+        hp = maxHp;
     }
     
-    public int getHp(){return hp;}
+    public int getHp(){
+        return hp;
+    }
     
-    public int getMaxHp(){return maxHp;}
+    public int getMaxHp(){
+        return maxHp;
+    }
     
-    public void takeDamage(int d){}
+    public void takeDamage(int d){
+        
+        if (hp>0){
+            if (hp>d) {    
+                hp-=d;
+            }
+        }
+        if(hp<d){
+            hp =0;
+        }  
+    }
     
-    public void heal(){}
+    public void heal(){ 
+        hp = maxHp;
+    }
     
-    public String getName(){return name;}
+    public String getName(){
+        return name;
+    }
     
-
-
     @Override
-    public String toString(){return (name + "HP: " + hp + "/" + maxHp);} 
+    public String toString(){return (name + " HP: " + hp + "/" + maxHp);} 
 
 }
-
