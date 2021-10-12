7eaafa001362db1fe81aba94b58360865aaa70e1
jessi <jessicale321@gmail.com>
2021-10-12 16:57:30 -0700

    Update Trainer.java

--- Trainer.java
+++ Trainer.java
@@ -1,16 +1,20 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.awt.Point;
+
 public class trainer extends Entity{ //FIXME
-    private String name; //not listed on UML
+    private String name; //not listed on UML, inherited from entity probs
     private int money;
     private int potions;
     private int pokeballs;
-//    private Point loc;
-//    private Map map;
-//    private ArrayList<Pokemon> pokemon;
+    private Point loc;
+    private Map map;
+    private ArrayList<Pokemon> pokemon;
 
-public trainer(String n, Pokemon p, Map m) { //FIXME
+public trainer(String n, Pokemon p, Map m) { //FIXME, super? //maxHp 25
     name = n;
-    //pokemon.add(p); //pokemon
-    //map
+    pokemon.add(p); //pokemon
+    map = m;
 }
 
 public int getMoney() {
@@ -44,8 +48,9 @@ public void receivePotion() {
     potions += 1;
 }
 
-public void usePotion(int pokeIndex) {
+public void usePotion(int pokeIndex) { //pokeIndex = loc in arraylist of pokemon the user wants to heal w/ potions
     potions -= pokeIndex; //FIXME
+    //Pokemon.heal(); //set the pokemon's hp to it's maxHp
 }
 
 public boolean hasPokeball() {
@@ -62,18 +67,31 @@ public void receivePokeball() {
 }
 
 public boolean catchPokemon(Pokemon p) {
-    return false;
+    if (pokeballs > 0) {
+        //get hp & maxHp of pokemon
+        //use hp to calc likelihood of it being caught
+        //randomize to determine if successful
+        //add pokemon to user's list of pokemon
+        return true;
+    }
+    return false; //if unsuccessful capture
 }
 
-public Point getLocation() {
-    return false;
+public Point getLocation() { //return where * is on map
+    return loc; 
 }
 
 public char goNorth() {
-    return 'n';
+    //loc change 
+    if (getCharAt(locabove).equals('x')) { //invalid
+        return 'x';
+    }
+    loc = getLocation(above); //update trainer loc
+    return 'n'; //tf is this - ask, do i just return what getCharAt is returning??
 }
 
 public char goSouth() {
+    //loc change
     return 's';
 }
 
@@ -93,8 +111,8 @@ public void healAllPokemon() {
     int x = 1;
 }
 
-public Pokemon getPokemon(int index) {
-    return Pokemon p;
+public Pokemon getPokemon(int index) { //rip get help, how to deal with abstract pokemon class - or not
+    return pokemon.get(index);
 }
 
 public String getPokemonList() {
