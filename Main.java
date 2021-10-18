0a1eaff159cfb8a707b30f41cca051461338e67e
jessi <jessicale321@gmail.com>
2021-10-18 16:38:48 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,13 +9,14 @@ public class Main {
         System.out.println("Prof. Oak: Hello there new trainer, what is your name? ");
         String input = in.nextLine();
         System.out.println("Great to meet you, " + input + "!");
+
         System.out.println("Choose your first pokemon:\n1. Charmander\n2. Bulbasaur\n3. Squirtle");
         int first = poke.nextInt();
         Pokemon n = null;
         if (first == 2){
             n = new Bulbasaur();
         }
-        trainer user = new trainer(input, n, Map);//take int input to choose pokemon
+        Trainer user = new Trainer(input, n, Map);//take int input to choose pokemon
     }
 
     public void mainMenu() {
@@ -30,45 +31,46 @@ public class Main {
         return random;
     }
 
-    public static void trainerAttack(trainer t, Pokemon wild) {
+    public static void trainerAttack(Trainer t, Pokemon wild) {
         System.out.println("A wild " + wild.getName() + " appeared!");
         Scanner in = new Scanner(System.in);
         while (wild.getHp() > 0){
             System.out.println(wild.getBasicMenu());
             int start = in.nextInt();
             if (start == 1){
-                System.out.println(wild.);
+                System.out.println(wild);
             }
-        }
-
     }
 
     public static void store(Trainer t) {
-       boolean quit = false;       
-       int money =  t.getMoney();
-        
-        do {
-            System.out.println("Hello! What can I help you with?");
-            System.out.println("1. Buy Potions - $5 ");
-            System.out.println("2. Buy Poke Balls - $3");
-            System.out.println("3. Exit");
-            int choice = CheckInput.getIntRange(1, 3);
-            switch (choice) {
-                   case 1:
-                       System.out.println("Here's your potion.");
-                       money-=5;
-                       break;
-                   case 2:
-                       System.out.println("Here's your Poke Balls");
-                       money-=3;
-                       break;
-                   case 3:
-                       System.out.println("Thank you, come again soon!");
-                       quit = true;
-                       break;
-                   default:
-                       break;
-               }
-           } while (!false);              
+        boolean quit = false;       
+        int money =  t.getMoney();
+         
+         do {
+             System.out.println("Hello! What can I help you with?");
+             System.out.println("1. Buy Potions - $5 ");
+             System.out.println("2. Buy Poke Balls - $3");
+             System.out.println("3. Exit");
+             int choice = CheckInput.getIntRange(1, 3);
+             switch (choice) {
+                    case 1:
+                        System.out.println("Here's your potion.");
+                        money-=5;
+                        break;
+                    case 2:
+                        System.out.println("Here's your Poke Balls");
+                        money-=3;
+                        break;
+                    case 3:
+                        System.out.println("Thank you, come again soon!");
+                        quit = true;
+                        break;
+                    default:
+                        break;
+                }
+            } while (!false);              
+     }
+
     }
 }
+
